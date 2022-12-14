89523182306ff3c387f6c09d32a39adc3c8ad31b
liam <ltyuanmu@gmail.com>
2022-12-14 18:07:51 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="header">
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+.header {
+  height: 6em;
+  background-image: linear-gradient(to top, #4C41F5 100%, #58AAF9 100%);
+}
+
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
