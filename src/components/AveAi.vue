ac61a4c074e22a507342c7b4a93596ef119ce0c0
mohaijiang <mohaijiang110@163.com>
2022-12-16 16:41:16 +0800

    api

--- src/components/AveAi.vue
+++ src/components/AveAi.vue
@@ -23,6 +23,28 @@
           </a-col>
         </a-row>
 
+        <a-row>
+          <a-col>
+            <a-descriptions title="风险" bordered >
+              <a-descriptions-item label="风险评估"> {{ risk.risk_score }} </a-descriptions-item>
+              <a-descriptions-item label="合约包含外部方法，可能被用于貔貅合约"> {{ risk.external_call }} </a-descriptions-item>
+              <a-descriptions-item label="可增发，存在恶意发行砸盘的可能"> {{risk.has_mint_method }} </a-descriptions-item>
+              <a-descriptions-item label="交易可暂停"> {{ risk.transfer_pausable}} </a-descriptions-item>
+              <a-descriptions-item label="看起来不像貔貅">{{ risk.is_honeypot }}</a-descriptions-item>
+              <a-descriptions-item label="合约已开源"> {{ risk.source_code? '开源': '未开源' }} </a-descriptions-item>
+              <a-descriptions-item label="所有者不能篡改余额">{{ risk.owner_change_balance }}</a-descriptions-item>
+              <a-descriptions-item label="看起来不像代理合约">{{ risk.is_proxy }}</a-descriptions-item>
+              <a-descriptions-item label="滑点费率不可修改"> {{risk.slippage_modifiable}} </a-descriptions-item>
+              <a-descriptions-item label="不存在白名单">{{ risk.has_white_method }}</a-descriptions-item>
+              <a-descriptions-item label="不存在黑名单">{{ risk.has_black_method }}</a-descriptions-item>
+              <a-descriptions-item label="已抛弃管理员权限">{{ risk.owner === '0x0000000000000000000000000000000000000000' }}</a-descriptions-item>
+              <a-descriptions-item label="池子已加锁">{{ risk.has_lock_method }}</a-descriptions-item>
+              <a-descriptions-item label="不能找回管理员权限">{{ risk.can_take_back_ownership }}</a-descriptions-item>
+              <a-descriptions-item label="无交易冷却机制">{{ risk.trading_cooldown }}</a-descriptions-item>
+            </a-descriptions>
+          </a-col>
+        </a-row>
+
       </a-card>
   </div>
 </template>
@@ -36,6 +58,7 @@ const network = ref('eth')
 const address = ref('0x111111111117dC0aa78b770fA6A738034120C302')
 const loading = ref(false)
 const issueNum = ref('')
+const risk = ref({})
 
 const query = () => {
   loading.value = true
@@ -46,7 +69,8 @@ const query = () => {
       "x-auth": '6359bc6122e3930be34a36319ec159a41671001252326238764'
     }
   }).then((data)=> {
-    console.log(data)
+    risk.value = data.data.data.token_contract.contract_data
+    console.log(risk.value)
   }).finally(()=> {
     loading.value = false
   })
--- src/components/GoPlus.vue
+++ src/components/GoPlus.vue
@@ -2,34 +2,28 @@
   <div>
       <a-card title="go-plus">
         <a-row>
-          <a-col span="8" style="text-align: right">
-            <a-select  v-model:value="network"  style="width: 120px">
-              <a-select-option value="56">BSC</a-select-option>
-              <a-select-option value="1">Ethereum</a-select-option>
-              <a-select-option value="128">HECO</a-select-option>
-              <a-select-option value="137">Polygon</a-select-option>
-              <a-select-option value="42161">Arbitrum</a-select-option>
-              <a-select-option value="10">Optimism</a-select-option>
-              <a-select-option value="43114">Avalanche</a-select-option>
-              <a-select-option value="250">Fantom</a-select-option>
-              <a-select-option value="66">OKC</a-select-option>
-              <a-select-option value="25">Cronos</a-select-option>
-              <a-select-option value="321">KCC</a-select-option>
-              <a-select-option value="100">Gnosis</a-select-option>
-              <a-select-option value="tron">Tron</a-select-option>
-            </a-select>
-          </a-col>
-          <a-col span="5">
-            <a-input v-model:value="address"  style="width: 240px"></a-input>
-          </a-col>
-          <a-col>
-            <a-button type="primary" @click="query" :loading="loading">query</a-button>
-          </a-col>
-        </a-row>
-        <a-row>
           <a-col>
             <a-descriptions title="issue" bordered >
               <a-descriptions-item label="number of issue">{{ issueNum }}</a-descriptions-item>
+              <a-descriptions-item label="可信任的代币">{{risk.trust_list}}</a-descriptions-item>
+              <a-descriptions-item label="合约已开源">{{ risk.is_open_source}}</a-descriptions-item>
+              <a-descriptions-item label="无代理合约">{{ risk.is_proxy }}</a-descriptions-item>
+              <a-descriptions-item label="未发现可取回所有权">{{ risk.can_take_back_ownership }}</a-descriptions-item>
+              <a-descriptions-item label="未发现Owner可改余额"> {{ risk.owner_change_balance }} </a-descriptions-item>
+              <a-descriptions-item label="未发现隐藏的owner"> {{ risk.hidden_owner }} </a-descriptions-item>
+              <a-descriptions-item label="该代币不能自毁"> {{ risk.selfdestruct}} </a-descriptions-item>
+              <a-descriptions-item label="未发现外部合约调用风险">{{ risk.external_call }}</a-descriptions-item>
+              <a-descriptions-item label="可能不是貔貅代币">{{ risk.is_honeypot }}</a-descriptions-item>
+              <a-descriptions-item label="无暂停交易功能"> {{ risk.transfer_pausable }}</a-descriptions-item>
+              <a-descriptions-item label="代币可全部卖出"> {{ risk.cannot_sell_all }} </a-descriptions-item>
+              <a-descriptions-item label="可以买入">{{ risk.cannot_buy }}</a-descriptions-item>
+              <a-descriptions-item label="无交易冷却功能">{{ risk.trading_cooldown }}</a-descriptions-item>
+              <a-descriptions-item label="交易数量不限"> {{ risk.is_anti_whale }}</a-descriptions-item>
+              <a-descriptions-item label="交易税不可改"> {{ risk.slippage_modifiable }}</a-descriptions-item>
+              <a-descriptions-item label="不包含黑名单机制">{{ risk.is_blacklisted}}</a-descriptions-item>
+              <a-descriptions-item label="不包含白名单机制"> {{ risk.is_whitelisted }}</a-descriptions-item>
+              <a-descriptions-item label="未发现可针对特定地址改税">{{ risk.personal_slippage_modifiable }}</a-descriptions-item>
+
             </a-descriptions>
           </a-col>
         </a-row>
@@ -38,22 +32,58 @@
 </template>
 
 <script setup lang="ts">
-import {defineComponent, onMounted, ref} from 'vue'
+import {defineComponent, onMounted, toRefs, ref, defineProps, computed} from 'vue'
 import axios from 'axios'
 
-const network = ref('56')
-const address = ref('0x111111111117dC0aa78b770fA6A738034120C302')
+const props = defineProps({
+  network:String,
+  address:String
+})
 const loading = ref(false)
 const issueNum = ref('')
+const risk = ref({})
+
+//    <a-select-option value="56">BSC</a-select-option>
+//    <a-select-option value="1">Ethereum</a-select-option>
+
+const networkVal = computed(()=> {
+  if (props.network.value === 'bsc'){
+    return 56
+  }else if (props.network.value === 'eth'){
+    return 1
+  }else {
+    return 1
+  }
+})
 
 const query = () => {
   loading.value = true
-  axios.get(`/goplus/api/v1/token_security/${network.value}?contract_addresses=${address.value}`).then(data => {
-    console.log(data)
+  console.log(props.address.value)
+  axios.get(`/goplus/api/v1/token_security/${networkVal.value}?contract_addresses=${props.address}`).then(data => {
+    // risk.value = data.data.result[`${address.value}`]
+    risk.value = Object.values(data.data.result)[0]
   }).catch(err => {
 
   }).finally(()=> {
     loading.value = false
+  })
+
+  loading.value = false
+}
+
+const foo = () =>{
+  console.log("foo")
+  console.log(props.address)
+  axios.post('/bitying/ceye/contract',
+      {
+        "uname":"biteagle",
+        "chain":2,
+        "address":"0xBBbbCA6A901c926F240b89EacB641d8Aec7AEafD",
+        "time":new Date().getTime(),
+        "sign":"fDaSGLxOsiG0lTbnVVicECexBeNMZFVtlChy+78Bb6gg+PSO3ZP5O9QkDtIkypxqQ4iXYOnNwuM4pL/juQqs8slMOID0SJZXkbZ60ZeJv2+4Y2bxM/BNxyRyltrNMnfokAe+cpliW4FWk49miapIvnRE8T9iclUgNQmRba+p9tt/505JyB8io6NfVDr/OnTc3wu5oa3GKyM/LxST+qFHP/wzphfORMdJ8fD4NdfM9dFwMQqyD5hkmXeGqiUjZ1E9Cngsm7bBPx1qBMcVS7HQEV9OPSJtsU1nil9w0lSb1t/tjsOQnY+Z4TycCLMZbluwhSAei6RjXf3fBSmrISpM6g=="
+      }
+  ).then(res => {
+    console.log(res.data)
   })
 }
 
@@ -61,6 +91,10 @@ onMounted(() => {
   console.log('onMounted')
 })
 
+defineExpose({
+  foo,
+  query
+});
 </script>
 
 <style scoped>
